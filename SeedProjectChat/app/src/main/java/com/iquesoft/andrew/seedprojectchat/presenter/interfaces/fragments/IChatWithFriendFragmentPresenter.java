22e4509284256735fe50df58247d25b0062010a2
AndruxaOsetrov@gmail.com <Arhangel2011>
2016-10-26 19:08:24 +0300

    Send photo in message and send file

--- SeedProjectChat/app/src/main/java/com/iquesoft/andrew/seedprojectchat/presenter/interfaces/fragments/IChatWithFriendFragmentPresenter.java
+++ SeedProjectChat/app/src/main/java/com/iquesoft/andrew/seedprojectchat/presenter/interfaces/fragments/IChatWithFriendFragmentPresenter.java
@@ -1,12 +1,8 @@
 package com.iquesoft.andrew.seedprojectchat.presenter.interfaces.fragments;
 
-import android.content.Context;
-import android.widget.EditText;
-
 /**
  * Created by andru on 9/15/2016.
  */
 
 public interface IChatWithFriendFragmentPresenter {
-    boolean onSendMessage(EditText messageField, Context context);
 }
