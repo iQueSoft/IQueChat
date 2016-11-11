33f8c9061e18d8cc02c22db10ea680e101b08830
Andrew <AndruxaOsetrov@gmail.com>
2016-11-11 11:19:42 +0200

    Started develop main screen

---
+++ SeedProjectChat/app/src/main/java/com/iquesoft/andrew/seedprojectchat/view/interfaces/fragments/IMainFragment.java
@@ -0,0 +1,10 @@
+package com.iquesoft.andrew.seedprojectchat.view.interfaces.fragments;
+
+import com.arellomobile.mvp.MvpView;
+
+/**
+ * Created by andru on 08.11.2016.
+ */
+
+public interface IMainFragment extends MvpView {
+}
