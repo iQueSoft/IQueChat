a5295bbe9ff2b14f31a724617605e9523747d3a7
KirilTairov <pyrosun@gmail.com>
2016-11-18 21:14:27 +0200

    Need fix PushNotification

--- SeedProjectChat/app/src/main/java/com/iquesoft/andrew/seedprojectchat/Receiver/PushReceiver.java
+++ SeedProjectChat/app/src/main/java/com/iquesoft/andrew/seedprojectchat/Receiver/PushReceiver.java
@@ -1,15 +1,14 @@
 package com.iquesoft.andrew.seedprojectchat.receiver;
 
 import com.backendless.push.BackendlessBroadcastReceiver;
-import com.iquesoft.andrew.seedprojectchat.services.PushServices;
 
 /**
  * Created by andru on 9/6/2016.
  */
 
 public class PushReceiver extends BackendlessBroadcastReceiver {
-    @Override
-    public Class<PushServices> getServiceClass() {
-        return PushServices.class;
-    }
+//    @Override
+//    public Class<PushServices> getServiceClass() {
+//        return PushServices.class;
+//    }
 }
