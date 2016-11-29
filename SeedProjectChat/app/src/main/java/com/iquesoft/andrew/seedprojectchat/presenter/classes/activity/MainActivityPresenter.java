c85852ddb80914e46e1b08eadf3e01fe88945867
KirilTairov <pyrosun@gmail.com>
2016-11-29 18:10:05 +0200

    Refactoring Main Activity

--- SeedProjectChat/app/src/main/java/com/iquesoft/andrew/seedprojectchat/presenter/classes/activity/MainActivityPresenter.java
+++ SeedProjectChat/app/src/main/java/com/iquesoft/andrew/seedprojectchat/presenter/classes/activity/MainActivityPresenter.java
@@ -1,22 +1,52 @@
 package com.iquesoft.andrew.seedprojectchat.presenter.classes.activity;
 
+import android.util.Log;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+import com.backendless.Backendless;
+import com.backendless.async.callback.BackendlessCallback;
+import com.iquesoft.andrew.seedprojectchat.Network.ApiCall;
+import com.iquesoft.andrew.seedprojectchat.common.DefaultsBackendlessKey;
 import com.iquesoft.andrew.seedprojectchat.presenter.interfaces.activity.IMainActivityPresenter;
 import com.iquesoft.andrew.seedprojectchat.view.interfaces.activity.IMainActivity;
 
-import javax.inject.Inject;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import rx.Observable;
 
 /**
  * Created by Andrew on 18.08.2016.
  */
+@InjectViewState
+public class MainActivityPresenter extends MvpPresenter<IMainActivity> implements IMainActivityPresenter {
 
-public class MainActivityPresenter implements IMainActivityPresenter {
-
-    private IMainActivity view;
-
-    @Inject
-    public MainActivityPresenter(IMainActivity view){
-        this.view = view;
+    @Override
+    protected void onFirstViewAttach() {
+        subscribeToMultipleChanelPushNotification(new LinkedList<>());
+        super.onFirstViewAttach();
     }
 
+    private void subscribeToMultipleChanelPushNotification(List<String> listId){
+        ApiCall.getGroupChatList().subscribe(response -> {
+            Observable.from(response).subscribe(respons -> listId.add(respons.getObjectId()));
+            ApiCall.getCurentFriendList().subscribe(friendses ->{
+                Observable.from(friendses).subscribe(respons -> listId.add(respons.getObjectId()));
+                subscribePushNotification(listId);
+            });
+        });
+    }
 
+    private void subscribePushNotification(List<String> chanels){
+        Date date = new Date();
+        date.setTime(date.getTime() + 31536000L);
+        Backendless.Messaging.registerDevice(DefaultsBackendlessKey.GOOGLE_PROJECT_ID, chanels, date, new BackendlessCallback<Void>() {
+            @Override
+            public void handleResponse(Void aVoid) {
+                Log.d("register", "register ok");
+            }
+        });
+    }
 }
