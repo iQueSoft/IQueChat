09712809fd47574f20c1ed32cd57e61b854585e7
ktairov <kirill.tairov@iquesoft.net>
2016-11-29 18:10:05 +0200

    Refactoring Main Activity

--- SeedProjectChat/app/src/main/java/com/iquesoft/andrew/seedprojectchat/view/interfaces/activity/IMainActivity.java
+++ SeedProjectChat/app/src/main/java/com/iquesoft/andrew/seedprojectchat/view/interfaces/activity/IMainActivity.java
@@ -1,8 +1,10 @@
 package com.iquesoft.andrew.seedprojectchat.view.interfaces.activity;
 
+import com.arellomobile.mvp.MvpView;
+
 /**
  * Created by Andrew on 18.08.2016.
  */
 
-public interface IMainActivity {
+public interface IMainActivity extends MvpView {
 }
