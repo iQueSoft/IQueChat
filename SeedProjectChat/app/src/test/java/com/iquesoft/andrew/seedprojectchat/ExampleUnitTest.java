cb41fe3f4f13ba5458f071812734b2bf01cfbbf0
KirilTairov <pyrosun@gmail.com>
2016-12-19 17:25:21 +0200

    Added social network aviability test  and Messages model unitTest

--- SeedProjectChat/app/src/test/java/com/iquesoft/andrew/seedprojectchat/ExampleUnitTest.java
+++ SeedProjectChat/app/src/test/java/com/iquesoft/andrew/seedprojectchat/ExampleUnitTest.java
@@ -1,6 +1,10 @@
 package com.iquesoft.andrew.seedprojectchat;
 
+import com.iquesoft.andrew.seedprojectchat.model.Messages;
+
 import org.junit.Test;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -14,5 +18,20 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void model_isCorrect() throws Exception{
+        Messages messages = new Messages();
+        assertNotNull(messages);
+        messages.setRead(true);
+        assertNotNull(messages.getRead());
+        messages.setTimestamp(new Date());
+        assertNotNull(messages.getTimestamp());
+        messages.setData("TestData");
+        assertNotNull(messages.getData());
+        assertEquals("TestData", messages.getData());
+        messages.setPublisher_id("Test");
+        messages.setMessage_id("Test");
     }
 }
